0851851b5fa4c3f607042f842c48729867a30b7e
Elochukwu3 <oguajuromauld5@gmail.com>
2024-07-02 07:55:34 +0100

    clean up  app

--- task-1/routes/index.js
+++ task-1/routes/index.js
@@ -1,43 +1,8 @@
-const express = require('express');
-const route = express.Router();
-const axios = require('axios');
-const dotenv = require('dotenv');
+const express = require('express')
+const route = express.Router()
+const getUser = require("../controller/userController")
 
-dotenv.config();
-
-const ipgeolocationApiKey = process.env.IPGEOLOCATION_API_KEY;
-const openWeatherMapApiKey = process.env.OPENWEATHERMAP_API_KEY;
-
-route.get('/api/hello', async (req, res) =>{
-  const visitorName = req.query.visitor_name || 'Guest';
-  const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-
-  try {
-    // Get location from IP
-    const ipGeolocationUrl = `https://api.ipgeolocation.io/ipgeo?apiKey=${ipgeolocationApiKey}&ip=${clientIp}`;
-    const locationResponse = await axios.get(ipGeolocationUrl);
-    const locationData = locationResponse.data;
-    const location = locationData.city || 'Unknown Location';
-
-    
-    const openWeatherMapUrl = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${openWeatherMapApiKey}`;
-    const weatherResponse = await axios.get(openWeatherMapUrl);
-    const weatherData = weatherResponse.data;
-    const temperature = weatherData.main.temp;
-
-    const response = {
-      client_ip: clientIp,
-      location: location,
-      greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${location}`
-    };
-
-    res.json(response);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred' });
-  }
-});
+route.get("/api/hello", getUser)
 
 
-
-module.exports = route;
+module.exports = route
